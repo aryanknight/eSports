7adb26142dce589bc0d1acf150b3d4201909d0f3
Aryan Knight <aryan313122@gmail.com>
2022-01-16 21:37:30 +0530

    matchs

--- src/Components/Match/Match.jsx
+++ src/Components/Match/Match.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Pic from '../../Images/pic1.png';
+import Pic2 from '../../Images/pic2.png';
+import Pic3 from '../../Images/pic3.png';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import "./Match.css";
 
 export default function Match() {
@@ -19,20 +22,86 @@ export default function Match() {
                     </div>
 
                     <div className="match-cont-1">
-                        <div className="match-box-1">
-                            <div className="match-box-1-img-cont">
-                                <img src={Pic} alt="" className="match-box-1-img" />
-                            </div>
-                            <div className="match-box-1-text-cont">
-                                <div className="match-text-1">
-                                    Assasin Esports
+                        <div className="match-cont-2">
+                            <div className="match-box-1">
+                                <div className="match-box-1-img-cont">
+                                    <img src={Pic} alt="" className="match-box-1-img" />
                                 </div>
-                                <div className="match-text-2">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                <div className="match-box-1-text-cont">
+                                    <div className="match-text-1">
+                                        Assasin Esports
+                                    </div>
+                                    <div className="match-text-2">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="match-box-2">
+                                <div className="match-time">
+                                    03:50
+                                </div>
+                                <div className="match-date">
+                                    1st April 2022
+                                </div>
+                                <div className="match-icons" >
+                                    <YouTubeIcon style={{fontSize:"60px"}}/>
                                 </div>
                             </div>
                         </div>
-                        <div className="match-box-2">5</div>
+
+                        <div className="match-cont-2">
+                            <div className="match-box-1">
+                                <div className="match-box-1-img-cont">
+                                    <img src={Pic2} alt="" className="match-box-1-img" />
+                                </div>
+                                <div className="match-box-1-text-cont">
+                                    <div className="match-text-1">
+                                        Theninja
+                                    </div>
+                                    <div className="match-text-2">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="match-box-2">
+                                <div className="match-time">
+                                    03:50
+                                </div>
+                                <div className="match-date">
+                                    1st April 2022
+                                </div>
+                                <div className="match-icons" >
+                                    <YouTubeIcon style={{fontSize:"60px"}}/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="match-cont-2">
+                            <div className="match-box-1">
+                                <div className="match-box-1-img-cont">
+                                    <img src={Pic3} alt="" className="match-box-1-img" />
+                                </div>
+                                <div className="match-box-1-text-cont">
+                                    <div className="match-text-1">
+                                        Bluereign
+                                    </div>
+                                    <div className="match-text-2">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="match-box-2">
+                                <div className="match-time">
+                                    03:50
+                                </div>
+                                <div className="match-date">
+                                    1st April 2022
+                                </div>
+                                <div className="match-icons" >
+                                    <YouTubeIcon style={{fontSize:"60px"}}/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
